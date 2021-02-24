7c4fe6e8e69811749b591a55a210a79643c3867e
alissongoulart <alissonbrenoemp@gmail.com>
2021-02-24 00:27:54 -0300

    create establishment entity

--- src/establishment/establishment.service.ts
+++ src/establishment/establishment.service.ts
@@ -1,15 +1,28 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 
+export class EstablishmentEntity {
+  razao_social: string;
+  nome_fantasia: string;
+  cnpj: string;
+  email: string;
+  telefone: string;
+  endereco: string;
+  cidade: string;
+  estado: string;
+  agencia: string;
+  conta: string;
+}
+
 export class EstablishmentRepository {
-  async getEstablishments(): Promise<any> {
-    //
+  async getEstablishments(): Promise<EstablishmentEntity> {
+    return new EstablishmentEntity();
   }
 }
 
 @Injectable()
 export class EstablishmentService {
   constructor(private establishmentRepository: EstablishmentRepository) {}
-  async getEstablishments(): Promise<any> {
+  async getEstablishments(): Promise<EstablishmentEntity> {
     return await this.establishmentRepository.getEstablishments();
   }
 }

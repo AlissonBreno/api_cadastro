7aeb467ac773fab4c7931155eab880adcd33212f
Alisson ABMG. Breno Maximo Goulart <alissongoulart@gac.local>
2021-02-24 10:29:38 -0300

    typeorm initial config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EstablishmentModule } from './establishment/establishment.module';
 
 @Module({
-  imports: [EstablishmentModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 15432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'teste_fitcard',
+      synchronize: true,
+      dropSchema: false,
+      logging: false,
+      entities: ['dist/**/entities/*.entity.js'],
+    }),
+    EstablishmentModule,
+  ],
   controllers: [],
   providers: [],
 })

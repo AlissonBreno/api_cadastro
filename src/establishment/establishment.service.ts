2e830cf174075960c2189d4cba26ed1e76ac565b
alissongoulart <alissonbrenoemp@gmail.com>
2021-02-24 00:15:05 -0300

    Ensure getEstablishment() pass over all the tests

--- src/establishment/establishment.service.ts
+++ src/establishment/establishment.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+
+export class EstablishmentRepository {
+  async getEstablishments(): Promise<any> {
+    //
+  }
+}
 
 @Injectable()
-export class EstablishmentService {}
+export class EstablishmentService {
+  constructor(private establishmentRepository: EstablishmentRepository) {}
+  async getEstablishments(): Promise<any> {
+    return await this.establishmentRepository.getEstablishments();
+  }
+}
--- src/establishment/tests/establishment.service.spec.ts
+++ src/establishment/tests/establishment.service.spec.ts
@@ -1,18 +1,54 @@
+import { InternalServerErrorException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { EstablishmentService } from '../establishment.service';
+import {
+  EstablishmentRepository,
+  EstablishmentService,
+} from '../establishment.service';
 
 describe('EstablishmentService', () => {
   let service: EstablishmentService;
+  let repository: EstablishmentRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EstablishmentService],
+      providers: [
+        EstablishmentService,
+        {
+          provide: EstablishmentRepository,
+          useFactory: () => ({
+            getEstablishments: jest.fn(),
+          }),
+        },
+      ],
     }).compile();
 
     service = module.get<EstablishmentService>(EstablishmentService);
+    repository = module.get<EstablishmentRepository>(EstablishmentRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getEstablishments()', () => {
+    it('should be throw if repository throw', async () => {
+      (repository.getEstablishments as jest.Mock).mockRejectedValue(
+        new InternalServerErrorException()
+      );
+      await expect(service.getEstablishments).rejects.toThrow(
+        new InternalServerErrorException()
+      );
+    });
+
+    it('should be not throw if repository returns', async () => {
+      await expect(service.getEstablishments).not.toThrow(
+        new InternalServerErrorException()
+      );
+    });
+
+    it('should be return when repository returns', async () => {
+      (repository.getEstablishments as jest.Mock).mockReturnValue([{}]);
+      expect(await service.getEstablishments()).toEqual([{}]);
+    });
   });
 });

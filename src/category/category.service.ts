831e142703deac98d906bedda77331aa6abfeeb3
alissongoulart <alissonbrenoemp@gmail.com>
2021-02-25 06:32:44 -0300

    create method createCategories in category service

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CategoryInput } from './dto/category-input.dto';
 
 export class CategoryEntity {
   categoria: string;
 }
 
 export class CategoryRepository {
-  async getCategories(): Promise<CategoryEntity> {
+  async getCategories(): Promise<CategoryEntity[]> {
+    return [new CategoryEntity()];
+  }
+
+  async createCategories(params: CategoryInput): Promise<CategoryEntity> {
     return new CategoryEntity();
   }
 }
@@ -13,7 +18,12 @@ export class CategoryRepository {
 @Injectable()
 export class CategoryService {
   constructor(private categoryRepository: CategoryRepository) {}
-  async getCategories(): Promise<any> {
-    return this.categoryRepository.getCategories();
+
+  async getCategories(): Promise<CategoryEntity[]> {
+    return await this.categoryRepository.getCategories();
+  }
+
+  async createCategories(params: CategoryInput): Promise<CategoryEntity> {
+    return await this.categoryRepository.createCategories(params);
   }
 }
--- src/category/tests/category.service.spec.ts
+++ src/category/tests/category.service.spec.ts
@@ -5,20 +5,28 @@ import { CategoryRepository, CategoryService } from '../category.service';
 describe('CategoryService', () => {
   let service: CategoryService;
   let repository: CategoryRepository;
+  let mockData;
 
   beforeEach(async () => {
+    const categoryRepositoryMock = {
+      getCategories: jest.fn(),
+      createCategories: jest.fn(),
+    };
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         CategoryService,
         {
           provide: CategoryRepository,
-          useFactory: () => ({ getCategories: jest.fn() }),
+          useFactory: () => categoryRepositoryMock,
         },
       ],
     }).compile();
 
     service = module.get<CategoryService>(CategoryService);
     repository = module.get<CategoryRepository>(CategoryRepository);
+    mockData = {
+      categoria: 'Supermercado',
+    };
   });
 
   it('should be defined', () => {
@@ -42,6 +50,27 @@ describe('CategoryService', () => {
     it('should be return when repository return', async () => {
       (repository.getCategories as jest.Mock).mockReturnValue([{}]);
       expect(await service.getCategories()).toEqual([{}]);
+    });
+  });
+
+  describe('createCategories()', () => {
+    it('should be throw if repository throw', async () => {
+      (repository.createCategories as jest.Mock).mockRejectedValue(
+        new InternalServerErrorException()
+      );
+      await expect(service.createCategories(mockData)).rejects.toThrow(
+        new InternalServerErrorException()
+      );
+    });
+
+    it('should be not throw if repository returns', async () => {
+      (repository.createCategories as jest.Mock).mockReturnValue(mockData);
+      expect(await service.createCategories(mockData)).toEqual(mockData);
+    });
+
+    it('should be called repository with correct params', async () => {
+      await service.createCategories(mockData);
+      expect(repository.createCategories).toBeCalledWith(mockData);
     });
   });
 });

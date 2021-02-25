0573eaf18d12637dacbe57ba2a5344a02b3b1873
alissongoulart <alissonbrenoemp@gmail.com>
2021-02-25 06:19:14 -0300

    create module category

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from './config/typeorm';
 import { EstablishmentModule } from './establishment/establishment.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeOrmConfig), EstablishmentModule],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig), EstablishmentModule, CategoryModule],
   controllers: [],
   providers: [],
 })

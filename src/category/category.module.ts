3a51cbb36aa251d16d9848ab7256b34e5daad318
Alisson ABMG. Breno Maximo Goulart <alissongoulart@gac.local>
2021-02-25 11:26:03 -0300

    fix typeorm imports

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
+import { CategoryEntity } from './entities/category.entity';
+import { CategoryRepository } from './category.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CategoryEntity, CategoryRepository])],
   providers: [CategoryService],
-  controllers: [CategoryController]
+  controllers: [CategoryController],
 })
 export class CategoryModule {}
--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -2,10 +2,12 @@ import { IsNotEmpty } from 'class-validator';
 import {
   Column,
   CreateDateColumn,
+  Entity,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 
+@Entity('category')
 export class CategoryEntity {
   @PrimaryGeneratedColumn()
   id: number;

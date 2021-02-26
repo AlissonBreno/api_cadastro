935f7a42cb0d5d1871d0be61a07cc067fef499eb
Alisson ABMG. Breno Maximo Goulart <alissongoulart@gac.local>
2021-02-26 14:21:31 -0300

    removing unused console.log

--- src/shared/helpers/agencia.helper.ts
+++ src/shared/helpers/agencia.helper.ts
@@ -1,8 +1,6 @@
 export function addMaskAgencia(agencia: string): string {
   if (agencia.length <= 4) {
-    console.log(agencia);
     agencia = agencia.replace(/^(\d{3})(\d{1})/, '$1-$2');
-    console.log(agencia);
   }
 
   return agencia;

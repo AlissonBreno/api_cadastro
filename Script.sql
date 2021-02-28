d1f3c87de901087c1d16d3ad4c120aeec3d01c0d
alissongoulart <alissonbrenoemp@gmail.com>
2021-02-28 16:42:52 -0300

    add script file

---
+++ Script.sql
@@ -0,0 +1,5 @@
+insert into category (categoria, url_icon) values ('Supermercado', 'store_mall_directory');
+insert into category (categoria, url_icon) values ('Restaurante', 'restaurant');
+insert into category (categoria, url_icon) values ('Borracharia', 'car_repair');
+insert into category (categoria, url_icon) values ('Posto', 'local_gas_station');
+insert into category (categoria, url_icon) values ('Oficina', 'car_repair');
